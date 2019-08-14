1de3df48dd5ca8f4af3315c45974d44785811b2b
Lukas Maximo Grilo Abreu Jardim <lukasmaximo@gmail.com>
2019-08-14 18:58:22 -0300

    Add files via upload

--- Assets/Scripts/Move_Wolf.cs
+++ Assets/Scripts/Move_Wolf.cs
@@ -6,19 +6,22 @@ using System.Timers;
 
 public class Move_Wolf : MonoBehaviour
 {
-    //float gravity = 10;
+    float gravity = 10;
     public float changeMove = 10.0f;
-    public float jump_factor = 30;
+    public float jump_factor = 6;
     public float rotateMove = 90.0f;
     public Rigidbody rgd;
-    public bool isGrounded;
+    private bool isGrounded;
     public GameObject wolf;
+    public MeshCollider mcl;
     public float jump_aux;
+    public Collider col;
 
     // Start is called before the first frame update
     void Start()
     {
         rgd = GetComponent<Rigidbody>();
+        mcl = GetComponent<MeshCollider>();
     }
 
     // Update is called once per frame
@@ -56,16 +59,27 @@ public class Move_Wolf : MonoBehaviour
         {
             Wolf_walk(0f, rotateMove);
         }
+        if (rgd.detectCollisions == false)
+        {
+            Console.Out.WriteLine("Is not a Collision");
+            float r = wolf.transform.position.y;
+            wolf.transform.Translate(new Vector3(0, -gravity,0)*Time.deltaTime);
+
+        }
+        else {
+            Console.Out.WriteLine("Is a Collision");
+            rgd.drag = 0;
+        }
         
     }
     void Jump() {
         jump_aux = jump_factor;
         transform.Translate(new Vector3(0, jump_aux, 0) * Time.deltaTime);
-        while (isGrounded == false)
+        /*while (isGrounded == false)
         {
             transform.Translate(new Vector3(0, jump_aux, 0) * Time.deltaTime);
             jump_aux -= jump_factor / 20;
-        }
+        }*/
     }
     void Wolf_walk(float x1, float x2) {
         if (x1 != 0.0f) {
@@ -79,11 +93,11 @@ public class Move_Wolf : MonoBehaviour
     }
     void Walk(float x1) { transform.Translate(Vector3.forward * x1 * Time.deltaTime); }
     void Round(float x2) { transform.Rotate(Vector3.up, x2 * Time.deltaTime); }
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision hit)
     {
         isGrounded = true;
     }
-    void OnCollisionExit(Collision collision)
+    void OnCollisionExit(Collision hit)
     {
         isGrounded = false;
     }
